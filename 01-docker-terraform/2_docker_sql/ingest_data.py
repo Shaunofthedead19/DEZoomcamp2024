f61db61a04c7394359c2a81555dc662968ea10f1
Shaunofthedead19 <52634252+Shaunofthedead19@users.noreply.github.com>
2024-02-18 19:57:51 +1100

    committing changes so far

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -9,6 +9,9 @@ from time import time
 import pandas as pd
 from sqlalchemy import create_engine
 
+from pyarrow.parquet import ParquetFile
+import pyarrow as pa
+
 
 def main(params):
     user = params.user
@@ -21,18 +24,24 @@ def main(params):
     
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+    # if url.endswith('.csv.gz'):
+    #     csv_name = 'output.csv.gz'
+    # else:
+    #     csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
+    file_name= 'output.parquet'
+
+    os.system(f"wget {url} -O {file_name}")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    # df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-    df = next(df_iter)
+    # df = next(df_iter)
+
+    pf = ParquetFile(file_name)
+    first_hun = next(pf.iter_batches(batch_size=100))
+    df = pa.Table.from_batches([first_hun]).to_pandas()
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -42,12 +51,14 @@ def main(params):
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
-    while True: 
+    one_mil_count = 0
+    while one_mil_count <= 9: 
 
         try:
             t_start = time()
             
-            df = next(df_iter)
+            # df = next(df_iter)
+            df = pa.Table.from_batches([next(pf.iter_batches(batch_size=100000))]).to_pandas()
 
             df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
             df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -57,6 +68,8 @@ def main(params):
             t_end = time()
 
             print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+            one_mil_count=one_mil_count+1
 
         except StopIteration:
             print("Finished ingesting data into the postgres database")
